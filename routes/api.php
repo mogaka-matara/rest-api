5b9df5f79720fc74e15db1759764e5d1d7c8c2bf
mogaka-matara <mogakakennedy254@gmail.com>
2024-09-06 16:31:17 +0300

    register Route update

--- routes/api.php
+++ routes/api.php
@@ -12,6 +12,7 @@ Route::resource('products', ProductController::class);
 
 //Public Routes
 Route::post('/register', [ AuthController::class, 'register']);
+Route::post('/login', [ AuthController::class, 'login']);
 Route::get('/products',[ProductController::class, 'index'] );
 Route::get('/products{id}',[ProductController::class, 'show'] );
 Route::get('/products/search/{name}', [ ProductController::class, 'search']);
